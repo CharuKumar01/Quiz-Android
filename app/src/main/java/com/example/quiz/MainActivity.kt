feaf7c9032e93fdea5f0d8bd3c04a61fe8f5332e
CharuKumar01 <kumarcharu83@gmail.com>
2025-01-24 23:29:53 +0530

    alert dialogue for back button

--- app/src/main/java/com/example/quiz/MainActivity.kt
+++ app/src/main/java/com/example/quiz/MainActivity.kt
@@ -3,9 +3,11 @@ package com.example.quiz
 import android.os.Bundle
 import androidx.activity.OnBackPressedCallback
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
 import com.example.quiz.databinding.ActivityMainBinding
 
@@ -14,7 +16,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        bind = DataBindingUtil.setContentView(this, R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -22,11 +24,20 @@ class MainActivity : AppCompatActivity() {
         }
         val navController = this.findNavController(R.id.navHost)
 
+        val backBtnDialog = AlertDialog.Builder(this)
+            .setTitle("Alert!")
+            .setMessage("Are you sure?")
+            .setPositiveButton("Yes"){ _, _ ->
+                finish()
+            }.setNegativeButton("No"){ dialog, _ ->
+                dialog.dismiss()
+            }.create()
+
         onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
             override fun handleOnBackPressed() {
                 if (!navController.popBackStack()) {
                     //If no fragments in the back stack, exit the activity
-                    finish()
+                    backBtnDialog.show()
                 }
             }
 
